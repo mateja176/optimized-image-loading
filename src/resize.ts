ccb1ac3fb9576aed0147c3de58071eb1f5d1b680
Mateja Petrovic <ma.petrovic@levi9.com>
2019-11-12 14:10:43 +0100

    feat: adds placeholder image generating script

---
+++ src/resize.ts
@@ -0,0 +1,8 @@
+import jimp from "jimp";
+
+jimp.read("neutron-star-collision.png").then(img =>
+  img
+    .resize(835, 546)
+    .quality(20)
+    .write("neutron-star-collision.placeholder.jpeg")
+);
